6baebcc0bf96fb692044b10519894adaa8f5bb04
Nolan Levi <cyberdevhq@proton.me>
2025-07-01 19:01:39 +0100

    Updated profile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import './../public/favicon.ico'
+
+
 
 export const metadata: Metadata = {
+  
   title: 'RedKernel | Offensive Security Journey',
   description: 'Built with Next.js & Tailwind CSS',
   generator: 'Next.js',
+  icons: {
+  icon: '/favicon.ico',
 }
+
+}
+
 
 export default function RootLayout({
   children,
